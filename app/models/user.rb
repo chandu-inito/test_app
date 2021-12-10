a251d091b9d83f88451d59107a19096d21044e15
chandu <sambangi@inito.com>
2021-12-10 17:22:59 +0530

    Add secure password using BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+
   before_save { self.email = email.downcase }
   has_many :articles
+
   validates :username, presence: true,
    uniqueness: {case_sensitive: false},
    length: {minimum: 3, maximum: 50}
+
   validates :email, presence: true,
     uniqueness: {case_sensitive: false},
     length: {maximum: 105}
+
+  has_secure_password
 end
